7f5320c3658ea55c7ff2be0136c81d948ea8a01a
Michael Emmi <michael.emmi@gmail.com>
2015-07-12 19:30:37 +0200

    Initial commit of Bibly.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bibly
+
+Bibly converts a sequence of DBLP keys into BibTeX entries.
---
+++ bibly.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'uri'
+require 'open-uri'
+require 'nokogiri'
+
+DB = 'http://dblp.uni-trier.de/rec/bibtex1/'
+SELECTOR = '#bibtex-section > pre'
+
+def get(key)
+  begin
+    Nokogiri::HTML(open(URI.join(DB,key))).css(SELECTOR).first.text.    gsub(/DBLP:/,'')
+  rescue OpenURI::HTTPError
+    puts "WARNING: no record found for key '#{key}''"
+    nil
+  end
+end
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{File.basename $0} [options] FILES"
+  opts.separator ""
+  opts.on("-h", "--help", "Show this message.") do
+    puts opts
+  end
+end.parse!
+
+ARGF.each do |key|
+  puts get(key.strip)
+end
